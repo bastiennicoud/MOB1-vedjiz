c5b20b5f8b0f4a1b85e6997efbeb74ff3f719f49
Bastien <bastien.nicoud@gmail.com>
2018-05-01 10:05:01 +0200

    Adding navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { NavController } from 'ionic-angular'
 import { HomePage } from '../pages/home/home';
+import { SearchPage } from '../pages/search/search';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  // Import navigation controller
+  @ViewChild(Nav) nav: Nav
+
+  // Sets the root page
+  rootPage:any = HomePage
+
+  // Array to declare all the app pages
+  pages: Array<{title: string, component: any}>
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +27,20 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    // Initialize all the pages
+    this.pages = [
+      { title: 'Search', component: SearchPage }
+    ]
+  }
+
+  /**
+   * Change the app page
+   *
+   * @param page 
+   */
+  openPage(page) {
+    this.nav.setRoot(page.component)
   }
 }
 
