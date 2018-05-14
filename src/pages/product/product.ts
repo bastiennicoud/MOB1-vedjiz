33052deb2a4e6a0a9028584869b2e5110ff9c199
Bastien <bastien.nicoud@gmail.com>
2018-05-14 14:58:04 +0200

    Add inputs for edition on products page

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Product } from '../../models/product';
 
 /**
@@ -18,12 +18,37 @@ export class ProductPage {
 
   private product: Product
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private modified: boolean
+
+  constructor(public toastCtrl: ToastController, public navCtrl: NavController, public navParams: NavParams) {
     this.product = this.navParams.get('product')
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProductPage');
+  }
+
+  valuesChanged() {
+    this.modified = true
+  }
+
+  ionViewCanLeave() {
+    if(this.modified) {
+      let toast = this.toastCtrl.create({
+        message: "Il faut sauver ou abandonner les modifications."
+      })
+      toast.present()
+    } else {
+      return true
+    }
+  }
+
+  save() {
+
+  }
+
+  abort() {
+
   }
 
 }
