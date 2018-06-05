eadda97815cfe9af04aad0fba1acf2f76f46eecc
Bastien <bastien.nicoud@gmail.com>
2018-06-05 08:13:25 +0200

    Check repo version

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -19,34 +19,38 @@ import { Http } from '@angular/http'
 export class DataProvider {
 
   constructor(public http: HttpClient, private storage: Storage, public platform: Platform) {
-    this.getFreshDatasFromApi()
+    this.getFreshDatas()
   }
 
+  /**
+   * Check if the local version on the datas is up to date
+   * If not, call the backend repo and update local storage
+   */
+  private getFreshDatas () {
+    this.http.get('http://vedjserver.mycpnv.ch/api/v1/lastupdate').subscribe(data => {
+      this.storage.get('last_repo_update').then(val => {
+        let localUpdate = new Date(val)
+        let repoUpdate = new Date(data.updated_at)
+        if (localUpdate.getTime() < repoUpdate.getTime()) {
+          this.getFreshDatasFromApi()
+          this.storage.set('last_repo_update', repoUpdate.getTime())
+        }
+      })
+    })
+  }
+
+  /**
+   * Get new products from backend
+   */
   public getFreshDatasFromApi () {
     this.http.get('http://vedjserver.mycpnv.ch/api/v1/vegetables').subscribe((data) => {
       this.storage.set('products', data)
     })
   }
 
-  public addProduct (product: Product) {
-    return new Promise<Array<Product>>((resolve, reject) => {
-      this.storage.get('products').then(val => {
-        val.push({
-          productName: product.getName(),
-          price:       product.getPrice(),
-          unitName:    product.getUnit(),
-          stock:       product.getStock(),
-          picture:     product.getPicturePath()
-        })
-        this.storage.set('products', val).then(() => {
-          resolve(val)
-        })
-      }).catch((err) => {
-        reject(err)
-      })
-    })
-  }
-
+  /**
+   * Retrive the products stored in the local storage
+   */
   public getProducts () {
     return new Promise<Array<Product>>((resolve, reject) => {
       this.storage.get('products').then(val => {
