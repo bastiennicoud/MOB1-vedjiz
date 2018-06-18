a65c7727286b6e3238668124b2c026118284913c
Bastien <bastien.nicoud@gmail.com>
2018-06-18 16:16:00 +0200

    Add synchro page

--- src/pages/synchro/synchro.ts
+++ src/pages/synchro/synchro.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
+import { Product } from '../../models/product'
+import { DataProvider } from '../../providers/data/data'
 
 /**
  * Generated class for the SynchroPage page.
@@ -15,11 +17,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SynchroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private products: Array<Product> = []
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private dataProvider: DataProvider) {
+    this.loadProducts()
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SynchroPage');
+    console.log('ionViewDidLoad SynchroPage')
+  }
+
+  private loadProducts () {
+    this.dataProvider.getProducts().then(data => {
+      this.products = data
+    })
+  }
+
+  private refreshVedjizz (e) {
+    this.dataProvider.refreshDatas().then(val => {
+      this.loadProducts()
+      e.complete()
+    })
   }
 
 }
